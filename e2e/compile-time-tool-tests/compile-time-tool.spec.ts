5de68e20046d6346d4e900b9af12007ef01ab774
Jaroslaw Glegola <jaroslaw.glegola@allegro.pl>
2021-06-06 16:05:52 +0200

    compile-time | Style fixes

--- e2e/compile-time-tool-tests/compile-time-tool.spec.ts
+++ e2e/compile-time-tool-tests/compile-time-tool.spec.ts
@@ -1,12 +1,12 @@
 import execa from 'execa';
 import { join } from 'path';
 
-const runTsStrictifyInPath = async (path: string): Promise<string> => {
+const runInPath = async (path: string): Promise<string> => {
   const cwd = process.cwd();
-  const tsStrictify = join(cwd, 'dist/compile-time-tool/cli.js');
+  const cli = join(cwd, 'dist/compile-time-tool/cli.js');
 
   process.chdir(path);
-  return execa('node', [tsStrictify], {
+  return execa('node', [cli], {
     env: {
       //the assertions break in an environment that supports color
       //override chalk color detection https://github.com/chalk/supports-color/blob/master/index.js
@@ -25,7 +25,7 @@ test('files are detected correctly', async () => {
   jest.setTimeout(20000);
   const path = process.cwd() + '/e2e/compile-time-tool-tests/repository';
 
-  await runTsStrictifyInPath(path).then((stdout) => {
+  await runInPath(path).then((stdout) => {
     expect(stdout).toMatch(/notOnPath.ts/);
     expect(stdout).toMatch(/onPath.ts/);
     expect(stdout).toMatch(/TS2532: Object is possibly 'undefined'/);
--- src/compile-time-tool/lib/strictFiles.ts
+++ src/compile-time-tool/lib/strictFiles.ts
@@ -5,18 +5,19 @@ import {
 } from './strictFilesUtils';
 import * as typescript from './typescript';
 
-const filterNodeModulesFiles = (files: string[]): string[] =>
+const filterOutNodeModulesFiles = (files: string[]): string[] =>
   files.filter((fileName) => !fileName.includes('/node_modules/'));
 
 const getFilesCheckedByTs = async (): Promise<string[]> => {
   const filesCheckedByTs = await typescript.listFilesOnly();
 
-  return filterNodeModulesFiles(filesCheckedByTs.split('\n'));
+  return filterOutNodeModulesFiles(filesCheckedByTs.split('\n'));
 };
 
 export const findStrictFiles = async (): Promise<string[]> => {
   const strictPaths = await getStrictFilePaths();
   const filesCheckedByTS = await getFilesCheckedByTs();
+
   const filesWithTsStrictComment = filterFilesWithStrictComment(filesCheckedByTS);
   const filesOnStrictPath = filesCheckedByTS.filter((fileName) => {
     return strictPaths.some((strictPath) => isFileOnStrictPath(fileName, strictPath));
