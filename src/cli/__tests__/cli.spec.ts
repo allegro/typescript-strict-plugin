a0c589a9f85c05e6acc1c0841577ec6343177182
Kamil Krysiak <kamkry99@gmail.com>
2022-01-06 13:26:53 +0100

    Adds process exit expects to tsc-strict root files

--- src/cli/__tests__/cli.spec.ts
+++ src/cli/__tests__/cli.spec.ts
@@ -2,6 +2,7 @@ import { findStrictErrors } from '../findStrictErrors';
 import { findStrictFiles } from '../findStrictFiles';
 import { getPluginConfig } from '../CliStrictFileChecker';
 import { run } from '../cli';
+import chalk from 'chalk';
 
 jest.mock('../CliStrictFileChecker', () => ({
   getPluginConfig: jest.fn(),
@@ -38,6 +39,7 @@ describe('tsc-strict root', () => {
     expect(console.log).toHaveBeenCalledWith(
       expect.stringMatching(/typescript-strict-plugin isn't configured in tsconfig.json/i),
     );
+    expect(process.exit).toHaveBeenCalledWith(1);
   });
 
   it('should display no strict files error', async () => {
@@ -52,6 +54,7 @@ describe('tsc-strict root', () => {
     expect(console.log).toHaveBeenCalledWith(
       expect.stringMatching(/Project does not contain any strict files/i),
     );
+    expect(process.exit).toHaveBeenCalledWith(1);
   });
 
   it('should display a correct number of found strict files and errors', async () => {
@@ -68,6 +71,7 @@ describe('tsc-strict root', () => {
     expect(console.log).toHaveBeenCalledWith(expect.stringMatching(/error2/i));
     expect(console.log).toHaveBeenCalledWith(expect.stringMatching(/error3/i));
     expect(console.log).toHaveBeenCalledWith(expect.stringMatching(/Found 3 errors/i));
+    expect(process.exit).toHaveBeenCalledWith(1);
   });
 
   it('should display a singular file and error', async () => {
@@ -81,5 +85,19 @@ describe('tsc-strict root', () => {
     // then
     expect(console.log).toHaveBeenCalledWith(expect.stringMatching(/Found 1 strict file/i));
     expect(console.log).toHaveBeenCalledWith(expect.stringMatching(/Found 1 error/i));
+    expect(process.exit).toHaveBeenCalledWith(1);
+  });
+
+  it('should display "all files passed" and should not call process exit 1', async () => {
+    // given
+    findStrictFilesMock.mockResolvedValue(['1.ts']);
+    findStrictErrorsMock.mockResolvedValue([]);
+
+    // when
+    await run();
+
+    // then
+    expect(console.log).toHaveBeenCalledWith(expect.stringContaining('All files passed'));
+    expect(process.exit).not.toHaveBeenCalledWith(1);
   });
 });
