da515f13aa48e860a9358ad6c9175dddd53ed4cd
KostkaBrukowa <jaroslaw.glegola@allegro.pl>
2022-10-14 12:04:45 +0200

    Fixes out of memory exception

--- e2e/tsc-strict/tsc-strict.spec.ts
+++ e2e/tsc-strict/tsc-strict.spec.ts
@@ -2,7 +2,11 @@ import execa from 'execa';
 import { join } from 'path';
 import { fixtureWithDefaultConfig, fixtureWithPathConfig } from '../fixtures/paths';
 
-const runInPath = async (path: string, args: string[] = []): Promise<string> => {
+const runInPath = async (
+  path: string,
+  args: string[] = [],
+  envArgs: Record<string, string> = {},
+): Promise<string> => {
   const cwd = process.cwd();
   const cli = join(cwd, 'dist/cli/tsc-strict/index.js');
 
@@ -12,6 +16,7 @@ const runInPath = async (path: string, args: string[] = []): Promise<string> => 
       //the assertions break in an environment that supports color
       //override chalk color detection https://github.com/chalk/supports-color/blob/master/index.js
       FORCE_COLOR: 'false',
+      ...envArgs,
     },
   })
     .then((response) => {
@@ -34,6 +39,20 @@ it('should detect strict file errors', async () => {
   expect(stdout).toMatch(/error TS2322: Type 'null' is not assignable to type 'string'\./i);
   expect(stdout).toMatch(/Found 1 strict file/i);
   expect(stdout).toMatch(/Found 1 error/i);
+});
+
+it('should detect heap out of memory exception', async () => {
+  //given
+  const { projectPath } = fixtureWithDefaultConfig;
+
+  // when
+  const stdout = await runInPath(projectPath, [], {
+    NODE_OPTIONS: '--max-old-space-size=20',
+  });
+
+  // then
+  expect(stdout).toMatch(/heap out of memory/i);
+  expect(stdout).toMatch(/Typescript task was aborted. Full error log/i);
 });
 
 it('should enable strict mode with a relative path config', async () => {
