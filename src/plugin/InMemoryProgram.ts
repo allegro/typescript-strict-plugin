4a1e355e9f1a6a801b02a2daaba02dcb2acd021b
Henrik Andersson <h@hnra.dev>
2023-12-15 20:58:19 +0100

    Fix erroneous diagnostics when importing from project references

--- src/plugin/InMemoryProgram.ts
+++ src/plugin/InMemoryProgram.ts
@@ -1,7 +1,20 @@
 import { PluginInfo } from './utils';
 import * as ts from 'typescript/lib/tsserverlibrary';
 
+/**
+ * djb2 hashing algorithm
+ * http://www.cse.yorku.ca/~oz/hash.html
+ */
+function generateDjb2Hash(data: string): string {
+  let acc = 5381;
+  for (let i = 0; i < data.length; i++) {
+    acc = (acc << 5) + acc + data.charCodeAt(i);
+  }
+  return acc.toString();
+}
+
 export class InMemoryProgram {
+  private host: undefined | ts.CompilerHost = undefined;
   private program: undefined | ts.SemanticDiagnosticsBuilderProgram = undefined;
 
   constructor(
@@ -9,10 +22,27 @@ export class InMemoryProgram {
     private readonly info: PluginInfo,
   ) {}
 
-  private getSourceFile(fileName: string) {
+  private getSourceFile(
+    fileName: string,
+    languageVersionOrOptions?: ts.ScriptTarget | ts.CreateSourceFileOptions,
+  ) {
     // Assume the file is canonicalized and absolute.
     const path = fileName as ts.Path;
-    const sourceFile = this.info.project.getSourceFile(path);
+    let sourceFile = this.info.project.getSourceFile(path);
+
+    // This case occurs when trying to resolve .d.ts outputs of project references.
+    if (!sourceFile && languageVersionOrOptions) {
+      sourceFile = this.host?.getSourceFile(fileName, languageVersionOrOptions);
+
+      if (sourceFile) {
+        // SemanticDiagnosticBuilder requires that the file has a version.
+        // We use the same hashing function that is used in the TypeScript repo as fallback.
+        (sourceFile as ts.SourceFile & { version: string }).version = generateDjb2Hash(
+          sourceFile.text,
+        );
+      }
+    }
+
     return sourceFile;
   }
 
@@ -24,12 +54,15 @@ export class InMemoryProgram {
 
     const rootFiles = [...currentProgram.getRootFileNames()];
     const options = { ...currentProgram.getCompilerOptions(), strict: true };
+    this.host = this._ts.createCompilerHost(options);
     const compilerHost: ts.CompilerHost = {
-      ...this._ts.createCompilerHost(options),
+      ...this.host,
 
       // Assume we can ignore the other arguments to these function.
-      getSourceFile: (fileName) => this.getSourceFile(fileName),
-      getSourceFileByPath: (fileName) => this.getSourceFile(fileName),
+      getSourceFile: (fileName, languageVersionOrOptions) =>
+        this.getSourceFile(fileName, languageVersionOrOptions),
+      getSourceFileByPath: (fileName, path, languageVersionOrOptions) =>
+        this.getSourceFile(fileName, languageVersionOrOptions),
     };
     const configFileParsingDiagnostics = currentProgram.getConfigFileParsingDiagnostics();
     const projectReferences = currentProgram.getProjectReferences();
