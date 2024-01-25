ec7d5a229cc92c3f9b07f5da1675398809840d36
Alex Guerra <alexander.guerra@braze.com>
2024-01-24 18:21:01 -0600

    Add in-memory strict language server

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,22 +1,29 @@
 import { PluginStrictFileChecker } from './PluginStrictFileChecker';
-import { log, PluginInfo, setupProxy, turnOffStrictMode, turnOnStrictMode } from './utils';
+import {
+  log,
+  PluginInfo,
+  setupLanguageServiceProxy,
+  setupStrictLanguageServiceHostProxy,
+} from './utils';
 import * as ts from 'typescript/lib/tsserverlibrary';
 
-const init: ts.server.PluginModuleFactory = () => {
+const init: ts.server.PluginModuleFactory = ({ typescript }) => {
   function create(info: PluginInfo) {
-    const proxy = setupProxy(info);
+    const proxy = setupLanguageServiceProxy(info);
+
+    const strictLanguageServiceHost = setupStrictLanguageServiceHostProxy(info);
+    const strictLanguageService = typescript.createLanguageService(strictLanguageServiceHost);
+
     log(info, 'Plugin initialized');
 
     proxy.getSemanticDiagnostics = function (filePath) {
       const strictFile = new PluginStrictFileChecker(info).isFileStrict(filePath);
 
       if (strictFile) {
-        turnOnStrictMode(info);
+        return strictLanguageService.getSemanticDiagnostics(filePath);
       } else {
-        turnOffStrictMode(info);
+        return info.languageService.getSemanticDiagnostics(filePath);
       }
-
-      return info.languageService.getSemanticDiagnostics(filePath);
     };
 
     return proxy;
